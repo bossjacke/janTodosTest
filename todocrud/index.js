bdb8c33bb5c9d6785535ccd62846fa897bde7811
jansan <bossjansan1234@gmail.com>
2025-10-24 14:40:21 +0530

    env

--- todocrud/index.js
+++ todocrud/index.js
@@ -50,7 +50,7 @@ app.put('/todos/:id', async(req,res)=>{
 
 
 app.listen(3000, () => {
-    mongoose.connect('mongodb://localhost:27017/todolist')
+    mongoose.connect('mongodb://localhost:27017/jansanTodoTest')
         .then(() => console.log("DB connected"))
         .catch(err => console.log(err));  
           console.log('server is running on http://localhost:3000/')
